1f596154ee4902b8b13f7dd1cc9972576e377c5b
tntra <tntra@DESKTOP-F6047KK>
2024-04-30 19:13:07 +0530

    minor change

---
+++ POC-RSA/POC-RSA/Services/CryptographyService.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography;
+
+namespace POC_RSA.Services
+{
+    public class CryptographyService
+    {
+        public CryptographyService()
+        {
+            using (var rsa = new RSACryptoServiceProvider(1024))
+            {
+                var publicKey = rsa.ToXmlString(false);
+                var privateKey = rsa.ToXmlString(true);
+            }
+
+        }
+
+        //public string GetPublicKey()
+        //{
+
+        //}
+
+        //public object Encrypt(string Plaintext)
+        //{
+
+        //}
+        //public object Decrypt(string Ciphertext)
+        //{
+
+        //}
+
+    }
+}
